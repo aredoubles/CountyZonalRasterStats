649640dd5f845635544888281c845599472e087b
Roger Shaw <roger.w.shaw@gmail.com>
2016-09-19 14:54:04 -0400

    CountyBuildReorganize
    Completed for individual counties, reorganized for fewer manual-fill-in
    needs.

--- CountyBuild.py
+++ CountyBuild.py
@@ -15,7 +15,7 @@ def TableNames(band, year):
     band = str(band)
     year = str(year)
     bandit = 'bio' + band       #Bio6, Bioclim
-    if len(band) = 1:
+    if len(band) == 1:
         band0 = 'bio0' + band   #Bio06, ?
     else: band0 = bandit        #Bio12, Bioclim
 
@@ -44,20 +44,8 @@ def BuildLyme(ctycode0):
     return lymebuild
 # Returns 'lymebuild', a dataframe of years x Lyme, county-code
 
-# Add lat/lon
-def LatLon (ctycode, lymebuild):      # ctycode = Bioclim, has lat/lon
-    yrs = range(2000,2017,1)
-    #thiscounty['county'] = ctycode
-    latlonquery = {}{}{}{}.format('''SELECT "Lat", "Lon" FROM "bio1.bio1-2000" WHERE "CtyID" = ''', "'", ctycode, "'")
-    latlonsql = pd.read_sql_query(latlonquery, con)
-    lymebuild['lat'] = latlonsql['Lat'][0]
-    lymebuild['lon'] = latlonsql['Lon'][0]
-    countychar = lymebuild
-    return countychar
-# Returns 'countychar', a dataframe that also includes lat/lon
-
 # Add Bioclim data
-def BioClim(ctycode, countychar):
+def BioClim(ctycode, lymebuild, ctycode0):
     for band in range(1,19,1):
         band0 = '{}{}{}'.format('bio', str(0), band)
         band = 'bio' + str(band)
@@ -72,22 +60,34 @@ def BioClim(ctycode, countychar):
             sql_query = '{} {} {} {} {} {}'.format('SELECT', yrvar, 'FROM', yrtab,
                                                 'WHERE "CtyID" =', ctystrquo)
             cell_from_sql = pd.read_sql_query(sql_query, con)
-            countychar.set_value(int(yr),band0,cell_from_sql.ix[0, yrvar[1:-1]])
-    #countychar.set_value(2015,'Year2',2015)
-    countychar.set_value(2015,'County',ctycode)
-    fullcounty = countychar
+            lymebuild.set_value(int(yr),band0,cell_from_sql.ix[0, yrvar[1:-1]])
+    lymebuild.set_value(2015,'county',ctycode0)
+    countybio = lymebuild
+    return countybio
+# returns 'countybio', a complete dataframe for a single county
+
+# Add lat/lon
+def LatLon (ctycode, countybio):      # ctycode = Bioclim, has lat/lon
+    quotecounty = '{}{}{}'.format("'", ctycode, "'")
+    latlonquery = '{}{}'.format('''SELECT "Lat", "Lon" FROM "bio1.bio1-2000" WHERE "CtyID" = ''', quotecounty)
+    latlonsql = pd.read_sql_query(latlonquery, con)
+    countybio['lat'] = latlonsql['Lat'][0]
+    countybio['lon'] = latlonsql['Lon'][0]
+    fullcounty = countybio
     return fullcounty
-# returns 'fullcounty', a complete dataframe for a single county
+# Returns 'fullcounty', a dataframe that also includes lat/lon
 
 # Send to CSV and SQL
 def Saveways(ctycode, fullcounty):
     namecsv = ctycode + '.csv'
     namesql = 'counties.' + ctycode
-    barnstable.to_csv(namecsv)
-    barnstable.to_sql(namesql, engine, if_exists='replace')
+    fullcounty.to_csv(namecsv)
+    fullcounty.to_sql(namesql, engine, if_exists='replace')
 
 '''
-Append to grand table, save to CSV and SQL
+# Append to grand table, save to CSV and SQL
+def GrandTable(ctycode, fullcounty):
+    lyme_data = pd.read_csv('lyme_CtyID.csv', index_col='CtyID')
 '''
 
 def main():
@@ -97,12 +97,9 @@ def main():
     ctycode = county + state                            # Barnstable25, Bioclim
     ctycode0 = '{}{}{}'.format(county, '0', state)      # Barnstable025, Lyme
     lymebuild = BuildLyme(ctycode0)
-    countychar = LatLon(ctycode, lymebuild)
-    fullcounty = BioClim(ctycode, countychar)
+    countybio = BioClim(ctycode, lymebuild, ctycode0)
+    fullcounty = LatLon(ctycode, countybio)
     Saveways(ctycode, fullcounty)
-
-    '''
-    Need to add other functions here, as part of main!
-    '''
+    # GrandTable(ctycode, fullcounty)
 
 main()
